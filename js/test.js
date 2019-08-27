fede76e27401d2591350e966095021000aa867b3
石老人 <1450982558@qq.com>
2019-08-27 16:51:30 +0800

    修改测试文件

--- js/test.js
+++ js/test.js
@@ -4,5 +4,5 @@
 var str1 = "https://www.baidu.com"
 str1 = str1.replace(/https/g,"http");
 console.log(str1);
-console.log("李祥雨".replace(/.(?=.)/g, '*'))
-console.log("李祥雨".replace(/(?<=.)./g, '*'))
+console.log("李祥雨".replace(/.(?=.)/g, '*'))//保留姓名最后一个字
+console.log("李祥雨".replace(/(?<=.)./g, '*'))//保留姓名第一个字
